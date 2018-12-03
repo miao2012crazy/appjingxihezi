726160fcf164aa104d18dec2234b517a3de633e3
miao2012crazy <miao2012crazy@163.com>
2018-12-03 20:29:12 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,95 @@
+var leftImg = [];//左容器图片
+var rightImg = [];//右容器图片
+Page({
+  data: {
+    images: [
+      {
+        id: '1',
+        src: '../../images/1.jpg',
+        name: '照片01',
+        data: '2017/11/1'
+      }, {
+        id: '2',
+        src: '../../images/2.jpg',
+        name: '照片02',
+        data: '2017/11/2'
+      }, {
+        id: '3',
+        src: '../../images/1.jpg',
+        name: '照片03',
+        data: '2017/11/3'
+      }, {
+        id: '4',
+        src: '../../images/2.jpg',
+        name: '照片04',
+        data: '2017/11/4'
+      }, {
+        id: '5',
+        src: '../../images/1.jpg',
+        name: '照片05',
+        data: '2017/11/5'
+      }, {
+        id: '6',
+        src: '../../images/2.jpg',
+        name: '照片06',
+        data: '2017/11/6'
+      }, {
+        id: '7',
+        src: '../../images/2.jpg',
+        name: '照片07',
+        data: '2017/11/7'
+      }, {
+        id: '8',
+        src: '../../images/1.jpg',
+        name: '照片08',
+        data: '2017/11/8'
+      }, {
+        id: '9',
+        src: '../../images/1.jpg',
+        name: '照片09',
+        data: '2017/11/9'
+      }, {
+        id: '10',
+        src: '../../images/1.jpg',
+        name: '照片10',
+        data: '2017/11/10'
+      }, {
+        id: '11',
+        src: '../../images/2.jpg',
+        name: '照片11',
+        data: '2017/11/11'
+      }, {
+        id: '12',
+        src: '../../images/2.jpg',
+        name: '照片12',
+        data: '2017/11/12'
+      }, {
+        id: '13',
+        src: '../../images/1.jpg',
+        name: '照片13',
+        data: '2017/11/13'
+      }, {
+        id: '14',
+        src: '../../images/1.jpg',
+        name: '照片14',
+        data: '2017/11/14'
+      }, {
+        id: '15',
+        src: '../../images/2.jpg',
+        name: '照片15',
+        data: '2017/11/15'
+      }, {
+        id: '16',
+        src: '../../images/2.jpg',
+        name: '照片16',
+        data: '2017/11/16'
+      }, {
+        id: '17',
+        src: '../../images/2.jpg',
+        name: '照片17',
+        data: '2017/11/17'
+      },
+
+    ]
+  },
+})
